df56017e430baf0ba27e6bf9b049505043fdcefd
Jatinthakur27 <155033781+Jatinthakur27@users.noreply.github.com>
2025-11-11 16:51:16 +0530

    Add files via upload

---
+++ Library/src/Shape.java
@@ -0,0 +1,16 @@
+ abstract class Shape {
+        abstract void draw(); // abstract method
+    }
+    class Circle extends Shape {
+        void draw() {
+            System.out.println("Drawing Circle");
+        }
+            public static void main(String[] args){
+
+
+                }
+            }
+
+
+
+
